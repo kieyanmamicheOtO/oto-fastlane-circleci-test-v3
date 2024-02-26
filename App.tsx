f0c4f5c02996acf14f99d4404a9b42cd2053d32c
kieyanmamicheOtO <kieyan.mamiche@otolawn.com>
2024-02-26 13:53:04 -0500

    maybe more files need to change to proc

--- App.tsx
+++ App.tsx
@@ -42,6 +42,7 @@ function Section({children, title}: SectionProps): JSX.Element {
         ]}>
         {title}
       </Text>
+      <Text>pls work</Text>
       <Text
         style={[
           styles.sectionDescription,
