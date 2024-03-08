32b9875b95f3c7511a33522f996386f161e280bc
kieyanmamicheOtO <kieyan.mamiche@otolawn.com>
2024-03-08 11:57:39 -0500

    Fixing automatic version increment in CI/CD

--- fastlane_helper_methods/date_transformer.rb
+++ fastlane_helper_methods/date_transformer.rb
@@ -1,6 +1,18 @@
 # Method to generate the version code / build number for fastfiles
-def transform_date_to_number
-    current_time = Time.now
-    formatted_date = current_time.strftime("%Y%m%d%H%M")
-    formatted_date.to_i
+def use_date_to_generate_number
+    
+    # Get the time OtO was founded (March 29, 2023)
+    time_march_29_2019_utc = Time.utc(2019, 3, 29, 0, 0, 0)
+
+    # Get the current time
+    current_time = Time.now.utc
+
+    # Calculate the difference in seconds
+    difference_seconds = current_time - time_march_29_2019_utc
+
+    # Convert seconds to minutes
+    difference_minutes = (difference_seconds / 60).to_i
+
+    # Convert to string and return
+    difference_minutes.to_s
 end
--- ios/fastlane/README.md
+++ ios/fastlane/README.md
@@ -13,17 +13,6 @@ For _fastlane_ installation instructions, see [Installing _fastlane_](https://do
 
 # Available Actions
 
-### test
-
-```sh
-[bundle exec] fastlane test
-```
-
-
-
-----
-
-
 ## iOS
 
 ### ios beta
