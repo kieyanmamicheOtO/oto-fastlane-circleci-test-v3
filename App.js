ca20c6e09e3be96d59b4ee86735e6ffbde5d252f
kieyanmamicheOtO <kieyan.mamiche@otolawn.com>
2024-03-25 14:58:54 -0400

    made step towards running eas update

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
-// import * as Updates from 'expo-updates';
+import * as Updates from 'expo-updates';
 
 export default function App() {
-  // const runTypeMessage = Updates.isEmbeddedLaunch
-  // ? 'This app is running from built-in code'
-  // : 'This app is running an update';
+  const runTypeMessage = Updates.isEmbeddedLaunch
+  ? 'This app is running from built-in code'
+  : 'This app is running an update';
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      {/* <Text>{runTypeMessage}</Text> */}
+      <Text>{runTypeMessage}</Text>
     </View>
   );
 }
