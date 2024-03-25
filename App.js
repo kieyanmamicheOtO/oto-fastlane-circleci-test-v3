c91f40f3633e4e46840f3e9f9fe7db19a12802e8
kieyanmamicheOtO <kieyan.mamiche@otolawn.com>
2024-03-25 12:13:47 -0400

    Testing eas update on different project

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+// import * as Updates from 'expo-updates';
+
+export default function App() {
+  // const runTypeMessage = Updates.isEmbeddedLaunch
+  // ? 'This app is running from built-in code'
+  // : 'This app is running an update';
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>{runTypeMessage}</Text> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
